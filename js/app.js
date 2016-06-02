c3b3ddd0efcb965e01106c7223e57d2eb6c255d6
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-02 12:26:47 -0400

    A new item is added to the front of the list instead of the back.

--- js/app.js
+++ js/app.js
@@ -9,21 +9,21 @@ $(document).foundation();
 
         buildList: function(studentName) {
           var li = document.createElement('li');
-        //  function deleteName(li) {this.li.parentNode.removeChild(this.li);}
-          li.innerHTML += studentName + '<a onclick="promote()" class="prom" href="#">Promote</a><a onclick="deleteName(li)" class="liAlign" href="#">   Delete</a>';
-          return li;
+          var p = document.createElement('p');
+          li.appendChild(p);
 
+        //  function deleteName(li) {this.li.parentNode.removeChild(this.li);}
+          p.innerHTML += studentName + '<a onclick="promote()" class="prom" href="#">Promote</a><a onclick="deleteName(li)" class="liAlign" href="#">   Delete</a>';
+          return li;
         },
 
         addValueToList: function(ev){
           ev.preventDefault();
           var studentName = this.studentName.value;
           var things = document.querySelector('.list');
-          things.appendChild(app.buildList(studentName));
-
-
+          //things.appendChild(app.buildList(studentName));
+          things.insertBefore(app.buildList(studentName), things.childNodes[0]);
       }
-
-
   };
+
 app.init();
