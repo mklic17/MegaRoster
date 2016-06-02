f72f906f11aef109446b2a84ec9ae0f7ddb397c1
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-02 16:36:50 -0400

    Changes made after class. I took some of the code out of certain methods and made new ones. Also I have the promote and remove button working properly.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,67 @@
 $(document).foundation();
 
-    var app = {
+    var MegaRoster = {
 
         init: function() {
+          this.setupEventListeners();
+          this.count = 0;
+        },
+
+        setupEventListeners: function(ev) {
           var x = document.querySelector('form');
-          x.onsubmit = this.addValueToList;
+          x.onsubmit = this.addValueToList.bind(this);
         },
 
         buildList: function(studentName) {
           var li = document.createElement('li');
-          var p = document.createElement('p');
-          li.appendChild(p);
-          p.innerHTML += studentName + '<a onclick="promote()" class="prom" href="#">Promote</a><a onclick="deleteName(li)" class="liAlign" href="#">Delete</a>';
+          var removeLink = this.buildLinkItem({
+            text: 'remove',
+            class: 'liAlign',
+            handler: function(){
+              li.remove();
+            },
+          });
+
+          var promoteLink = this.buildLinkItem({
+            text: 'promote',
+            class: 'prom',
+            handler: function(){
+              li.style.border = '2px green dashed';
+            },
+          });
+
+          li.innerText = studentName;
+          li.appendChild(removeLink);
+          li.appendChild(promoteLink);
+
           return li;
+        },
+
+        buildLinkItem: function(options) {
+          var a = document.createElement('a');
+          a.href = '#';
+          a.innerText = options.text;
+          a.onclick = options.handler;
+          a.className = options.class;
+          return a;
         },
 
         addValueToList: function(ev){
           ev.preventDefault();
-          var studentName = this.studentName.value;
+          var f =ev.currentTarget; //the thing that you are listening to the event on
+          var studentName = f.studentName.value;
+          var listItem = this.buildList(studentName);
+          //things.insertBefore(listItem, things.childNodes[0]);
           var things = document.querySelector('.list');
-          things.insertBefore(app.buildList(studentName), things.childNodes[0]);
-      }
+          this.prependChild(things, listItem);
+          this.count += 1;
+          f.reset();
+          f.studentName.focus()
+      },
+      prependChild: function (parent, child){
+        parent.insertBefore(child, parent.firstChild);
+      },
+
   };
 
-app.init();
+MegaRoster.init();
