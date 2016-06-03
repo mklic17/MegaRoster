90552eb647e1181bb2dacdf4904efe26afe44761
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-02 20:06:55 -0400

    The move to Top button is now working.

--- js/app.js
+++ js/app.js
@@ -12,56 +12,90 @@ $(document).foundation();
           x.onsubmit = this.addValueToList.bind(this);
         },
 
+        addValueToList: function(ev){
+          ev.preventDefault();
+          //debugger;
+          var f =ev.currentTarget; //the thing that you are listening to the event on
+          var studentName = f.studentName.value;
+          var listItem = this.buildList(studentName);
+          //things.insertBefore(listItem, things.childNodes[0]);
+          var things = document.querySelector('#ulList');
+          this.prependChild(things, listItem);
+          this.count += 1;
+          f.reset();
+          f.studentName.focus()
+      },
+
         buildList: function(studentName) {
           var li = document.createElement('li');
           var removeLink = this.buildLinkItem({
-            text: 'remove',
-            class: 'liAlign',
+            text: 'Remove',
+            css: 'liAlign',
             handler: function(){
               li.remove();
             },
           });
 
           var promoteLink = this.buildLinkItem({
-            text: 'promote',
-            class: 'prom',
+            text: 'Promote',
+            css: 'prom',
             handler: function(){
-              li.style.border = '2px green dashed';
+              li.style.backgroundColor = '#ADD8E6';
+            },
+          });
+          var topLink = this.buildLinkItem({
+            text: 'Top',
+            css: 'TopOfList',
+            handler: function(){
+              //debugger;
+              var things = li.parentNode;
+              MegaRoster.prependChild(things, li);
+
             },
           });
 
+          var upOne = this.buildLinkItem({
+            text:'+1',
+            css:'MoveUp',
+            handler: function() {
+              var things = li.parentNode;
+              var temp = li.previousSibling;
+              things.insertBefore(li, temp);
+            }
+
+          });
+          var downOne = this.buildLinkItem({
+            text: '-1',
+            css: 'MoveUp',
+            handler: function() {
+              var things = li.parentNode;
+              var temp = li.nextSibling;
+              things.insertBefore(temp, li);
+
+            }
+          });
+
           li.innerText = studentName;
+          li.appendChild(topLink);
           li.appendChild(removeLink);
           li.appendChild(promoteLink);
+          li.appendChild(upOne);
+          li.appendChild(downOne);
 
           return li;
         },
-
         buildLinkItem: function(options) {
           var a = document.createElement('a');
           a.href = '#';
           a.innerText = options.text;
           a.onclick = options.handler;
-          a.className = options.class;
+          a.className = options.css;
           return a;
         },
 
-        addValueToList: function(ev){
-          ev.preventDefault();
-          var f =ev.currentTarget; //the thing that you are listening to the event on
-          var studentName = f.studentName.value;
-          var listItem = this.buildList(studentName);
-          //things.insertBefore(listItem, things.childNodes[0]);
-          var things = document.querySelector('.list');
-          this.prependChild(things, listItem);
-          this.count += 1;
-          f.reset();
-          f.studentName.focus()
-      },
-      prependChild: function (parent, child){
-        parent.insertBefore(child, parent.firstChild);
-      },
-
+        prependChild: function (parent, child){
+          parent.insertBefore(child, parent.firstChild);
+        },
   };
 
 MegaRoster.init();
